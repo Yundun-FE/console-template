bb6ce05c9884dbd630242662c6b36cddf65f7acc
xank <xank@qq.com>
2018-11-21 14:08:02 +0800

    init

---
+++ config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"https://easy-mock.com/mock/5bf4ca1d09b4fb0dcd94f708/yundun"',
+})
